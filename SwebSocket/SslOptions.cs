728ec90e08a2cdb1a57e2c8186f750d480396241
Luca Schlecker <luca.schlecker@hotmail.com>
2024-04-26 20:26:24 +0200

    implement WebSocketBuilder

---
+++ SwebSocket/SslOptions.cs
@@ -0,0 +1,87 @@
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SwebSocket;
+
+public delegate Stream SslStreamFactory(Stream stream);
+
+public class SslOptions
+{
+    internal SslStreamFactory SslStreamFactory { get; private set; }
+
+    internal SslOptions(SslStreamFactory sslStreamFactory) => SslStreamFactory = sslStreamFactory;
+
+    public static SslOptions NoSsl => new SslOptions(stream => stream);
+
+    public static SslOptions ForServer(X509Certificate2 identity) => new SslOptions(stream =>
+    {
+        var sslStream = new SslStream(stream, true);
+        sslStream.AuthenticateAsServer(identity);
+        return sslStream;
+    });
+
+    public static SslOptions ForClient(string targetHost, RemoteCertificateValidationCallback callback) => new SslOptions(stream =>
+    {
+        var sslStream = new SslStream(stream, true, callback);
+        sslStream.AuthenticateAsClient(targetHost);
+        return sslStream;
+    });
+
+    public static SslOptions ForClient(string targetHost) => new SslOptions(stream =>
+    {
+        var sslStream = new SslStream(stream, true);
+        sslStream.AuthenticateAsClient(targetHost);
+        return sslStream;
+    });
+
+    public static SslOptions ForClient(string targetHost, X509Certificate2Collection caCertificates) => new SslOptions(stream =>
+    {
+        var remoteCallback = RemoteCertificateValidationCallbackWithCaCertificates(caCertificates);
+        var sslStream = new SslStream(stream, true, remoteCallback);
+        sslStream.AuthenticateAsClient(targetHost);
+        return sslStream;
+    });
+
+    private static RemoteCertificateValidationCallback RemoteCertificateValidationCallbackWithCaCertificates(X509Certificate2Collection caCertificates) => (sender, certificate, chain, sslPolicyErrors) =>
+    {
+        if (sslPolicyErrors == SslPolicyErrors.None)
+            return true;
+
+        if (sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateNameMismatch))
+            return false;
+
+        chain.ChainPolicy.ExtraStore.AddRange(caCertificates);
+
+        if (chain.Build(new X509Certificate2(certificate)))
+            return true;
+
+        if (chain.ChainStatus.Any(status => status.Status != X509ChainStatusFlags.UntrustedRoot))
+            return false;
+
+
+        foreach (var element in chain.ChainElements)
+        {
+            foreach (var status in element.ChainElementStatus)
+            {
+                if (status.Status == X509ChainStatusFlags.UntrustedRoot)
+                {
+                    var skip = false;
+
+                    foreach (var ca in caCertificates)
+                        skip |= ca.RawData.SequenceEqual(element.Certificate.RawData);
+
+                    if (skip)
+                        continue;
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    };
+}
--- SwebSocket/WebSocketBuilder.cs
+++ SwebSocket/WebSocketBuilder.cs
@@ -1,4 +1,53 @@
 
+using System;
+using System.Net.Sockets;
+
 namespace SwebSocket;
 
-public class WebSocketBuilder { }
+public class WebSocketBuilder
+{
+    private Uri? uri;
+    private SslOptions sslOptions = SslOptions.NoSsl;
+
+    public WebSocketBuilder To(string url) => To(new Uri(url));
+    public WebSocketBuilder To(Uri uri)
+    {
+        this.uri = uri;
+        if (uri.Scheme != "ws" && uri.Scheme != "wss")
+            throw new ArgumentException("Invalid scheme. Expected 'ws' or 'wss'.");
+        if (uri.Scheme == "wss")
+            return UseSsl();
+        else
+            return this;
+    }
+
+    public WebSocketBuilder UseSsl(SslOptions? sslOptions)
+    {
+        if (sslOptions == null) return UseSsl();
+        this.sslOptions = sslOptions;
+        return this;
+    }
+
+    public WebSocketBuilder UseSsl()
+    {
+        if (uri == null)
+            throw new InvalidOperationException("Cannot build WebSocket without a URL.");
+
+        sslOptions = SslOptions.ForClient(uri.Host);
+        return this;
+    }
+
+    public WebSocket Build()
+    {
+        if (uri == null)
+            throw new InvalidOperationException("Cannot build WebSocket without a URL.");
+
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect(uri.Host, uri.Port);
+        var stream = sslOptions.SslStreamFactory.Invoke(new NetworkStream(socket));
+        var frameSocket = new FrameSocket(socket, stream, true);
+        var handshake = new ClientHandshake(stream, uri.Host, (ushort)uri.Port, uri.AbsolutePath);
+        var ConnectionFrameSocket = new ConnectionFrameSocket(frameSocket, handshake);
+        return new WebSocket(ConnectionFrameSocket);
+    }
+}
