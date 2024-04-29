bf3d86aaf1cea924cf661d3e1e5ad2447d0a4946
Luca Schlecker <luca.schlecker@hotmail.com>
2024-04-29 13:51:42 +0200

    implement Listener

---
+++ SwebSocket/Listener.cs
@@ -0,0 +1,40 @@
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SwebSocket;
+
+public class Listener
+{
+    public bool Pending => listener.Pending();
+
+    private TcpListener listener;
+    private SslOptions sslOptions = SslOptions.NoSsl;
+
+    public Listener(IPAddress address, ushort port)
+    {
+        listener = new TcpListener(address, port);
+        listener.Start();
+    }
+
+    public Listener UseSsl(SslOptions sslOptions)
+    {
+        this.sslOptions = sslOptions;
+        return this;
+    }
+
+    public WebSocket Accept()
+    {
+        var socket = listener.AcceptSocket();
+        var stream = sslOptions.SslStreamFactory.Invoke(new NetworkStream(socket));
+        var frameSocket = new FrameSocket(socket, stream, false);
+        var connectionFrameSocket = new ConnectionFrameSocket(frameSocket, new ServerHandshake(stream));
+        return new WebSocket(connectionFrameSocket);
+    }
+
+    public bool TryAccept(out WebSocket? webSocket)
+    {
+        webSocket = Pending ? Accept() : null;
+        return webSocket != null;
+    }
+}
